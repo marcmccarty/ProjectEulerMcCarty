0040afbefbfb51067d4a0dae3fc46ddfc852e702
Marc McCarty <mccartym2@nku.edu>
2018-05-15 16:55:49 -0500

    Create Problem003.cs
    Problem 003 requests that we find the largest prime factor of an arbitrarily high number.

---
+++ Problem003.cs
@@ -0,0 +1,38 @@
+using System;
+
+namespace ProjectEulerWorkspace
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            double ourNum = 600851475143;
+
+            double highestPossibleFactor = Math.Sqrt(ourNum);
+
+            double maxFactor = 0;
+
+            for (int i = 2; i < (int)highestPossibleFactor; i++)
+            {
+                bool prime = true;
+                double highestPFPrime = Math.Sqrt(i);
+
+                for (int j = 2; j < (int)highestPFPrime; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        prime = false;
+                    }
+                }
+
+                if (prime && (ourNum % i == 0))
+                {
+                    maxFactor = i;
+                }
+            }
+            
+            Console.WriteLine(maxFactor);
+            Console.ReadLine();
+        }
+    }
+}
